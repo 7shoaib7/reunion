bb4cace026e5b5ea78787eb021357ee49429b713
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2022-11-28 16:35:55 +0530

    navbar-added

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./styles.css";
+import Button from '@mui/material/Button';
+import {useState} from "react"
+
+const Navbar = () => {
+  const [navText,setNavText] = useState("")  
+  const [navContent,setNavContent] = useState(["Rent","Sell","Buy"])
+    return (
+        <div className="navbar">
+            <div className="navbar-content">
+                <div className="navbar-log">
+                    {/* <img scr={} alt="logo" /> */}
+                    <span style={{color: 'darkslateblue',fontWeight:"600",fontSize:"1.5rem"}}>Estatery</span>
+                </div>
+                {navContent.map((item)=>(
+                    <div className={navText===item ? "navbar-text-onclick  navbar-content-box":"navbar-text-content navbar-content-box"}>
+                       <span onClick={()=>setNavText(item)}>{item}</span>
+                    </div>
+                ))}
+         
+            </div>
+            <div className="navbar-cta">
+                <Button  size="small" variant="outlined" style={{marginRight:"1rem"}} className="login">Login</Button>
+                <Button  size="small" variant="contained" className="signup">Sign up</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
